c733376398cbe852e6a299890342a11548f4cf3a
Ben Daniels <benjamindaniels21@gmail.com>
2022-06-15 21:53:28 -0400

    post request finished and logs to console

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  let firstName = req.body.fName;
+  let lastName = req.body.lName;
+  let email = req.body.email;
+
+  console.log(firstName, lastName, email);
 });
 
 app.listen(3000, function () {
