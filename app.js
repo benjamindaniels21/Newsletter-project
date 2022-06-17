8225c1b03d8547c6300a531ce4e469d1890151bc
Ben Daniels <benjamindaniels21@gmail.com>
2022-06-16 22:07:00 -0400

    working on incorporating mailchimp API

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
 const app = express();
+const https = require("https");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -11,13 +12,46 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  let firstName = req.body.fName;
-  let lastName = req.body.lName;
-  let email = req.body.email;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-  console.log(firstName, lastName, email);
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+  const url = "https://us18.api.mailchimp.com/3.0/01f0d7c1de";
+  const options = {
+    method: "POST",
+    auth: "ben1:8754221ae7b122491a5ac3c78c7cb521-us18",
+  };
+
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(3000, function () {
   console.log("The server is running on port 3000");
 });
+
+// List Id
+// 01f0d7c1de
+
+// API Key
+// 8754221ae7b122491a5ac3c78c7cb521-us18
